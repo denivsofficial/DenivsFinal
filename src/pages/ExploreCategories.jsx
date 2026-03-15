0f27a41b8714872d1ba6d08192e8b1b813b43611
Anuragkite <anuragkite143@gmail.com>
2026-03-15 12:09:58 +0530

    updated app routes and added hero in home.jsx

---
+++ src/pages/ExploreCategories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExploreCategories = (props) => {
+  return (
+    <div className='w-100% h-100% bg-amber-700'>
+   
+    </div>
+  )
+}
+
+export default ExploreCategories
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,20 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
-import Calculator from '../pages/Calculator';
-import ExploreCategories from '../pages/ExoloreCategories';
-import FeaturedList from '../pages/FeaturedList';
-import MoreProperty from '../pages/MoreProperty';
-import Footer from '@/pages/Footer';
+// import Calculator from '../pages/Calculator';
+// import ExploreCategories from '../pages/ExoloreCategories';
+// import FeaturedList from '../pages/FeaturedList';
+// import MoreProperty from '../pages/MoreProperty';
+// import Footer from '@/pages/Footer';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/calculator" element={<Calculator />} />
-      <Route path="/explore-categories" element={<ExploreCategories />} />
-      <Route path="/featured-listings" element={<FeaturedList />} />
-      <Route path="/more-properties" element={<MoreProperty />} />
-      <Route path="/footer" element={<Footer />} />
+      {/* <Route path="/calculator" element={<Calculator />} /> */}
+      {/* <Route path="/explore-categories" element={<ExploreCategories />} /> */}
+      {/* <Route path="/featured-listings" element={<FeaturedList />} /> */}
+      {/* <Route path="/more-properties" element={<MoreProperty />} /> */}
+      {/* <Route path="/footer" element={<Footer />} /> */}
     </Routes>
   );
 };
